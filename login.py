70a56108235e0b1733bafc77b88df8a707320108
manogna-45 <manognapoloji@gmail.com>
2026-05-16 13:46:35 +0530

    Add files via upload

---
+++ login.py
@@ -0,0 +1,7 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "manogna" and password == "tanush":
+    print("Login successful!")
+else:
+    print("Login failed")
