5fa84245d5dfff576f8a22d9a262b19ff9783dca
manogna-45 <manognapoloji@gmail.com>
2026-05-21 17:35:25 +0530

    Create evenoroddfun.py

---
+++ evenoroddfun.py
@@ -0,0 +1,8 @@
+num = int(input("Enter a number: "))
+def even_odd(number):
+    if number % 2 == 0:
+        print(number, "is even")
+    else:
+         print(number, "is odd")
+    
+even_odd(num)
